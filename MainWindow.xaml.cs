4ffcd586871bd074e736956cfa6796ee2d3a0625
Jeffrey Ganulin <45239282+jtganulin@users.noreply.github.com>
2022-12-10 13:52:12 -0500

    Modified UI for simplicity (WIP), and enabled users to remove files from selection, and to open file selection dialog via dbl-click on file listbox. Some improvements to source organization, error handling, and minor README change.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,92 +1,107 @@
 ﻿using System.Windows;
 using System.IO;
 using System.Text.RegularExpressions;
+using MessageBox = System.Windows.Forms.MessageBox;
 
-namespace MultiFileTextReplacer
+namespace MultiFileTextReplacer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private static OpenFileDialog selectFilesDialog = new();
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
+
+        //Style/Assign properties of file selection dialog
+        selectFilesDialog.Filter = "Text Files|*.txt|All File Types|*.*";
+        selectFilesDialog.Multiselect = true;
+        selectFilesDialog.Title = "Select Files";
+    }
+
+    private void ShowFileSelectDialog(object sender, EventArgs e)
+    {
+		//Show the dialog
+		if (selectFilesDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+		    //After items are selected, output them to the fileSelectionBox
+		    foreach (string fileName in selectFilesDialog.FileNames)
+		    {
+			    fileSelectionBox.Items.Add(fileName);
+		    }
         }
-    
-        private OpenFileDialog selectFilesDialog = new();
-        
-#region Add Files
-        public void addFiles_Click(object sender, RoutedEventArgs e)
+	}
+
+    public void AddFiles_Click(object sender, RoutedEventArgs e)
+    {
+        ShowFileSelectDialog(sender, e);
+    }
+
+    // Clear Selection Box and Clear FileNames
+    public void ClearFiles_Click(object sender, RoutedEventArgs e)
+    {
+        selectFilesDialog.Reset();
+        fileSelectionBox.Items.Clear();
+        //MessageBox.Show("All Files Cleared. You may add more using the Add File(s) Button.", "Files Cleared", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    #region Replace Text in Specified Files and Close Streams
+    public void btnGo_Click(object sender, RoutedEventArgs e)
+    {
+		if (selectFilesDialog.FileNames.Length == 0)
+		{
+			MessageBox.Show("No files selected. You may select files using the Add File(s) Button.", "No Files Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return;
+		}
+
+		foreach (string file in selectFilesDialog.FileNames)
         {
-            //Style/Assign Properties
-            selectFilesDialog.Filter = "Text Files|*.txt|All File Types|*.*";
-            selectFilesDialog.Multiselect = true;
-            selectFilesDialog.Title = "Select Files";
-            //Show the dialog
-            selectFilesDialog.ShowDialog();
-            //Hide the label defining selection
-            lblSelectInfo.Opacity = 0;
-            //After items are selected, output them to the fileSelectionBox
-            foreach (string fileName in selectFilesDialog.FileNames)
+            try
             {
-                fileSelectionBox.Items.Add(fileName);
+                using StreamReader reader = new(file);
+                using StreamWriter writer = new(file);
+
+                string content = reader.ReadToEnd();
+                content = Regex.Replace(content, findBox.Text, replaceBox.Text);
+                writer.Write(content);
             }
-        }
-#endregion
-
-#region Clear Selection Box and Clear FileNames
-        public void clearFiles_Click(object sender, RoutedEventArgs e)
-        {
-            selectFilesDialog.Reset();
-            fileSelectionBox.Items.Clear();
-            System.Windows.Forms.MessageBox.Show("All Files Cleared. You may add more using the Add File(s) Button.", "Files Cleared", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            //Make the selection help lbl visible again until new selection
-            lblSelectInfo.Opacity = 100;
-       
-        }
-        #endregion
-
-#region Replace Text in Specified Files and Close Streams
-        public void btnGo_Click(object sender, RoutedEventArgs e)
-        {
-            foreach (string file in selectFilesDialog.FileNames)
+            catch(Exception ex)
             {
-                try
-                {
-                    StreamReader reader = new StreamReader(file);
-                    string content = reader.ReadToEnd();
-                    reader.Close();
-                    content = Regex.Replace(content, findBox.Text, replaceBox.Text);
-                    StreamWriter writer = new StreamWriter(file);
-                    writer.Write(content);
-                    writer.Close();
-                }
-                catch
-                {
-                    // The user lacks appropriate permissions to read files, discover paths, etc. 
-                    System.Windows.Forms.MessageBox.Show("There has been an error. Please try again.");
-                };
-            }
-            
-            if (selectFilesDialog.FileNames.Length == 0)
-            {
-                System.Windows.Forms.MessageBox.Show("No Files Selected. You may select files using the Add File(s) Button.", "No Files Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else {
-                System.Windows.Forms.MessageBox.Show("Replacing Complete!\nYou may now exit the application or perform more tasks.", "Tasks Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+                MessageBox.Show("There has been an error. Please try again. \n" + ex.Message.ToString());
+                return;
+            };
         }
-        #endregion
 
-        private void aboutBtn_Click(object sender, RoutedEventArgs e)
+        MessageBox.Show("Replacing Complete!\nYou may now exit the application or perform more tasks.", "Replacing Completed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+    #endregion
+
+    private void aboutBtn_Click(object sender, RoutedEventArgs e)
+    {
+        Window aboutWin = new AboutWindow();
+        aboutWin.ShowDialog();
+    }
+
+    private void fileSelectionBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+		ShowFileSelectDialog(sender, e);
+	}
+
+    // Upon "Delete" press we'd like to remove the currently selected files
+    private void fileSelectionBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        if (e.Key == System.Windows.Input.Key.Delete)
         {
-            Window aboutWin = new AboutWindow();
-            aboutWin.ShowDialog();
-            
-        }
-    };
+            var selectedItems = fileSelectionBox.SelectedItems.OfType<string>().ToList();
+            for (int i = fileSelectionBox.Items.Count - 1; i >= 0;  i--)
+            {
+                if (selectedItems.Contains(fileSelectionBox.Items[i])) 
+                    fileSelectionBox.Items.RemoveAt(i);
+            }
 
-}
-
+		} else return;
+    }
+};
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Multi-File Text Replacer
-This program replaces a specified text string with a new one throughout multiple files.
+This program replaces a specified string with a new one throughout multiple text files.
 
 Created using WPF, upgraded from .NET Framework 4.0 to .NET 6.0
